997238d288eaee86529ce7d578ecadceb087d558
cesarorozco9628 <paulocesarorozcoguerrero@hotmail.com>
2023-07-21 07:07:08 -0600

    js

--- src/js/main.js
+++ src/js/main.js
@@ -39,7 +39,7 @@ const put_params_bulding = (data) => {
 
     div_ifrm.innerHTML += `
      <iframe 
-            src="http://novauat.morgana.mx3/quote/first_step/${params.length>0 ? `?${params}`:''}"
+            src="http://novauat.morgana.mx/quote/first_step/${params.length>0 ? `?${params}`:''}"
             style="width:100%; height:550px;"
         >
         </iframe>
